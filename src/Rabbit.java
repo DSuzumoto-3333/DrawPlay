5b6e360628e2e324ef9690de9a9151043673a643
Derek Suzumoto <dsuzumoto@mymail.mines.edu>
2021-02-18 17:46:09 -0700

    Added Rabbit.java class to draw a rabbit. TODO: add to paintComponent method in MyDrawwing.java - Derek

---
+++ src/Rabbit.java
@@ -0,0 +1,57 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+
+public class Rabbit {
+	//Head size
+	private static final int HEAD_DIMENSION = 75;
+	// eyes will be about 1/4 from top of head and 1/4 from left
+	private static final int EYE_Y = HEAD_DIMENSION/4;
+	private static final int EYE_X = HEAD_DIMENSION/4;
+	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
+	// pick eye dimensions
+	private static final int EYE_HEIGHT = 15;
+	private static final int EYE_WIDTH = 15;
+	// pick mouth height, width is based on head dimension
+	private static final int MOUTH_HEIGHT = 3;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION/8;
+	// mouth starts about 40% from left edge of head
+	private static final int MOUTH_X = HEAD_DIMENSION/10 * 5;
+	private static final int MOUTH_Y = HEAD_DIMENSION/10 * 7;
+	//Ears for rabbit
+	private static final int LEFT_EAR_X = HEAD_DIMENSION/6;
+	private static final int LEFT_EAR_Y = HEAD_DIMENSION* 2 / 5;
+	private static final int EAR_SEPARATION = 2 * HEAD_DIMENSION / 3;
+	private static final int EAR_WIDTH = 20;
+	private static final int EAR_HEIGHT = 80;
+	
+	// draw will render the Cat on the Graphics object
+	public void draw(Graphics g, int catX, int catY)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		int x=catX;
+		int y=catY;
+		// Draw the head
+		g2.setColor(Color.white);
+		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
+		// Draw the eyes
+		g2.setColor(Color.yellow);
+		x = catX + EYE_X; 
+		y = catY + EYE_Y;
+		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
+		x += EYE_SEPARATION;
+		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
+		// Draw the mouth
+		g2.setColor(Color.pink);
+		x = catX + MOUTH_X;
+		y = catY + MOUTH_Y;
+		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);	
+		
+		g2.setColor(Color.white);
+		x = catX + LEFT_EAR_X;
+		y = catY - LEFT_EAR_Y;
+		g2.fillArc(x, y, EAR_WIDTH, EAR_HEIGHT, 0, 180);
+	}
+}
